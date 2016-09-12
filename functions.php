4d7856c72c97b71c8b60535805e769e3d62b5cfa
Matthew Cascardi <mcascardi@users.noreply.github.com>
2016-09-12 13:40:11 -0700

    Added generic functions.php

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Functions.php
+ *
+ */
+
+/**
+ * Theme URI
+ *
+ * Alias for  get_stylesheet_directory_uri()
+ *
+ * @return string
+ */
+function theme_uri()
+{
+    $path = get_stylesheet_directory_uri();
+    $start = strpos($path, '//');
+    if ($start !== false) {
+        return substr($path, $start);
+    }
+    return $path;
+}
+
+/**
+ * Theme Path
+ *
+ * Alias for  get_stylesheet_directory()
+ *
+ * @return string
+ */
+function theme_path()
+{
+    return get_stylesheet_directory();
+}
+
