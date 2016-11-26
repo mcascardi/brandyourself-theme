03c4bfbb13fc96eeebd4038eab94ecd43be82c96
Matt Cascardi <mattcascardi@gmail.com>
2016-11-26 12:08:50 -0800

    Replaced paths on images with relative path ../img/* Indented HTML

---
+++ front-page.php
@@ -0,0 +1,90 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Matt Cascardi, Computer Software</title>
+    <meta name="description" content="Matt Cascardi, Computer Software: The authoritative source of Matt Cascardi's personal information, links, and social activity.">
+    <?php wp_head(); ?> 
+    <!--		<link rel="stylesheet" href="/js/app.css" type="text/css">  -->
+    <link rel="stylesheet" href="<?php echo theme_uri(); ?>/css/professional.css" type="text/css" />	
+    <link rel="stylesheet" href="<?php echo theme_uri(); ?>/css/blue.css" type="text/css" />		
+    
+    <!--		<script type="text/javascript" charset="UTF-8" src="/scripts/app.20160912160526.js"></script>	-->	
+    <!--[if lt IE 9]>
+	<script src="/scripts/iecompat/IE9.js" type="text/javascript"></script>
+	<script src="/scripts/iecompat/ie7-squish.js" type="text/javascript"></script>
+	<script src="/scripts/iecompat/html5.js"></script>
+	<![endif]-->
+    <link rel="image_src" href="http://user.photos.s3.amazonaws.com/user_18691.jpg" />
+    <link rel="shortcut icon" href="favicon.ico" />
+  </head>
+  <body>
+    <div id="header">
+      <ul id="nav">
+	<li class="current"><a href="/">Profile</a></li><li><a href="http://photos.mattcascardi.com/">Photography</a></li><li><a href="http://blog.mattcascardi.com">Blog</a></li>			
+      </ul>
+    </div>
+    <div id="wrapper">
+      <div class="vcard">
+	<div id="sideBar">
+	  <div id="portrait">
+	    <img src="http://user.photos.s3.amazonaws.com/user_18691.jpg" alt="Matt Cascardi" class="photo">
+	  </div>
+	  <div id="sideBarInfo">
+	    <h2 class="fn"><a href="/" rel="author">Matt Cascardi</a></h2>
+	    
+	    <h3 class="role"></h3>
+	    
+	    <h3 class="industry">Computer Software</h3>
+	    
+	    <div class="contactInfo">
+	      <ul>
+		<li class="adr">
+		  <span class="region">
+		    US								</span>
+		</li>
+	      </ul>
+	    </div>
+	    <h3>Social Networks</h3>
+	    <ul>
+	      <li class="facebook"><a href="https://www.facebook.com/matt.cascardi" rel="me" target="_blank">Facebook</a></li>
+	      <li class="linkedin"><a href="http://www.linkedin.com/in/mattcascardi" rel="me" target="_blank">LinkedIn</a></li>
+	    </ul>
+	  </div>
+	</div>	<div class="mainContent">
+	  <div id="about">
+	    <h1 class="fn">Matt Cascardi</h1>
+	    <h2>Matt Cascardi:  Web Developer, Server Administrator, and Bassist</h2>
+	    <hr>
+	    <p>Matt Cascardi was born and raised in Oakland, CA.  From an early age his parents started him in music lessons on violin and piano.  This began a lifelong love for music and the craft of playing an instrument that Matt fuels to this day.  At Montera Middle School he began playing tenor saxophone, and continued it through the jazz band at St. Mary's College High School.  During that time Matt became interested in playing bass and has gone on to becoming the bassist for 4 bands, 2 of which are still active.</p>&#13;
+<p><span style="line-height: 1.5em;">Matthew's other interests have been centered around computers and technology.  He has professional experience with website design and maintenance, as well as server administration. Specialties include standards-compliant design, CMS administration, and web-related technical support.</span></p>		</div>
+	</div>
+	<div class="mainContent">
+	  <h3>Matt Cascardi&#39;s Background</h3>
+	  
+	  <h4>Matt Cascardi&#39;s Experience</h4>
+	  <div id="experience">
+	    
+	    <h3><span  class="title">Web Developer</span> at <span  class="affiliation">Advance Analysis Inc.</span></h3><h5>May 2011 - Present</h5>
+	    <h3><span  class="title">Web Developer</span> at <span  class="affiliation">VentureBeat</span></h3><h5>January 2011 - Present</h5><p>Front-end and back-end work on Venturebeat.com and events.venurebeat.com.  Current tasks include Wordpress site administration and plugin authoring.</p>
+	    <h3><span  class="title">Web Designer</span> at <span  class="affiliation">ProFastServe</span></h3><h5>January 2009 - Present</h5><p>I build and customize websites for individuals, businesses, and other groups.</p>
+	    <h3><span  class="title">Academic Coach</span> at <span  class="affiliation">Extreme Learning</span></h3><h5>December 2008 - June 2009</h5><p>I tutored 1st-5th graders who were below basic learning levels at an after-school program in the Bay Area.</p>
+	    <h3><span  class="title">Intern</span> at <span  class="affiliation">Octagon Consulting</span></h3><h5>September 2007 - January 2009</h5><p>Internship duties involve marketing, front-end web design, and personell management.</p>
+	    <h3><span  class="title">Team Leader</span> at <span  class="affiliation">RGIS</span></h3><h5>January 2007 - October 2008</h5><p>Conduct inventory in retail and grocery stores.</p>
+	    <h3><span  class="title">Telemarketer</span> at <span  class="affiliation">P.F. Reilly</span></h3><h5>June 2005 - November 2006</h5><p>Generated leads for home remodel contractor.  Created telemarket and canvassing training tools.  Disbatched leads to salesmen.  Worked a booth at trade shows.</p>
+	    <h3><span  class="title">Assistant Computer Technician</span> at <span  class="affiliation">UC Berkeley, Chancellor's Office</span></h3><h5>May 2001 - August 2001</h5><p>Answered Help Desk calls regarding software issues, and troubles with email and calendar programs.  Tracked calls to Help Desk and verified completion of tasks.   Gathered inventory information of on- and off-site computer equipment.  Performed system installs from image.</p>
+	    <h3><span  class="title">Intern</span> at <span  class="affiliation">Imtech Inc.</span></h3><h5>January 2000 - June 2000</h5><p>Archived data and assisted with creating web graphics.</p>		</div>
+	  <h4>Matt Cascardi's Education</h4>
+	  <div id="education">			
+	    
+	    <h3>California State University-Chico</h3><h5>2003 – 2007</h5><br >
+	    <h3>Saint Mary's College High School</h3><h5>1999 – 2002</h5><br >		</div>
+	  <h4>Matt Cascardi&#39;s Interests &amp; Activities</h4>
+	  <div id="interests">
+	    <p>I like healthy food, organic and local if possible. Eating well is something I try to focus the remainder of my attention on.  Cooking is such a great social bonding activity as well, and the more food we can grow locally and organically the better.</p>
+	  </div>
+	</div>
+      </div>
+      <div class="spacer"></div>
+    </div>
+<?php get_footer(); ?>
