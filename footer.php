6c6f94dbfd3bac43e5e990c974dd7025446d0610
Matt Cascardi <matt@mattcascardi.com>
2019-01-27 11:32:04 -0700

    Made the theme more generic, removed personal references to myself Updated copyright in footer Made site title dynamic

--- footer.php
+++ footer.php
@@ -2,35 +2,23 @@
 <div id="footer">
     <div id="footerCenter">
         <div id="socialNetworks">
-	    <!--
-            <?php dynamic_sidebar('footer-1');?>
-
-	    <h4>Social Networks</h4>
-	    <ul>
-		<li class="facebook">
-		    <a href="https://www.facebook.com/matt.cascardi" rel="nofollow" target="_blank">Facebook</a>
-		</li>
-		<li class="linkedin">
-		    <a href="http://www.linkedin.com/in/mattcascardi" rel="nofollow" target="_blank">LinkedIn</a>
-		</li>
-	    </ul>
-	    -->
+            <?php
+                if ( is_active_sidebar('footer-1') ) {
+                    dynamic_sidebar('footer-1');
+		}
+             ?>
 	</div>
 	<div class="contactInfo vcard">
-	    <!--
-	    <?php dynamic_sidebar('footer-2');?>
-	    <h4><span class="fn">Matt Cascardi</span></h4>
-	    <ul>
-		<li class="adr">
-		    El Cerrito, California
-		</li>
-	    </ul>
-	    -->
+	    <?php
+		if ( is_active_sidebar('footer-2') ) {
+		    dynamic_sidebar('footer-2');
+		}
+	      ?>
 	</div>
     </div>
 </div>
 <div id="bottomAd">
-    &copy; 2016 Matt Cascardi
+   <?php printf(__('Website Design &copy; 2016 %s', 'brandyourself-theme'), 'Matt Cascardi'); ?>
 </div>
 <?php wp_footer(); ?>
   </body>
